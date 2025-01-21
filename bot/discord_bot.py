3806a02eb47ed164f68abd07fa628d24d923cdca
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2025-01-21 05:08:37 +0200

    Refactor DiscordBot to load settings from configuration files and add welcome message functionality

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -6,15 +6,16 @@ import ssl
 import asyncio
 import json
 import os
+import random
 
-# Path to the bot configuration file
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config", "settings.json")
+# Path to the config directory
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config")
 
-# Load the bot configuration from json
-bot_config = None
-with open(CONFIG_PATH, 'r') as f:
-    bot_config = json.load(f)
+# Path to the bot setting file
+SETTINGS_FILE_PATH = os.path.join(CONFIG_PATH, "settings.json")
 
+# Path to the welcome messages file
+WELCOME_MESSAGES_FILE_PATH = os.path.join(CONFIG_PATH, "welcome_messages.txt")
 
 class DiscordBot(commands.Bot):
     """Discord bot class to interact with the Discord API."""
@@ -33,7 +34,10 @@ class DiscordBot(commands.Bot):
         self.logger = logging.getLogger("DiscordBot") # Logger instance for the bot with the name "DiscordBot"
 
         self.lock = asyncio.Lock() # Async lock to prevent concurrent message sending
-        self.bot_config = bot_config # Bot configuration settings
+        self.bot_settings = None # Bot configuration settings
+        with open(SETTINGS_FILE_PATH, 'r') as f:
+            self.bot_settings = json.load(f)
+
 
     async def on_message(self, message: str) -> None:
         """Respond to messages that mention the bot."""
@@ -51,23 +55,34 @@ class DiscordBot(commands.Bot):
 
     async def on_ready(self) -> None:
         """Log information when the bot is ready."""
+        await self.change_presence(
+            status=discord.Status.online,  # Set the bot status to online
+            activity=discord.CustomActivity(name="Welcoming new members and checking for new products from stores") # Set the bot activity
+        )
+
         self.logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
 
 
     async def send_new_items(self, store_name_format: str, new_products: list) -> None:
         """Send new products to a specific Discord channel."""
-
         async with self.lock: # Ensure only one task can send messages at a time
-            channel = discord.utils.get(self.get_all_channels(), name=self.bot_config['new_items_channel_name'])
-            if not channel:
-                self.logger.warning(f"Channel '{self.bot_config['new_items_channel_name']}' not found.")
+
+            new_items_channel = None # Channel to send new items
+
+            for channel in self.get_all_channels():
+                if isinstance(channel, discord.TextChannel) and self.bot_settings['new_items_channel_name'].lower() in channel.name.lower():
+                    new_items_channel = channel
+                    break
+
+            if not new_items_channel:
+                self.logger.warning(f"Channel '{self.bot_settings['new_items_channel_name']}' not found.")
                 return
 
             if not new_products:
                 return
 
             # Get the embed color from the configuration
-            embed_color = self.bot_config.get('embed_color', (214, 140, 184))
+            embed_color = self.bot_settings.get('embed_color', (214, 140, 184))
 
             if isinstance(embed_color, list) and len(embed_color) == 3:
                 embed_color = discord.Color.from_rgb(*embed_color)
@@ -83,7 +98,7 @@ class DiscordBot(commands.Bot):
             )
 
             # Send section title
-            await channel.send(embed=embed)
+            await new_items_channel.send(embed=embed)
             await asyncio.sleep(0.5)
 
             # Send each product as a separate message
@@ -110,7 +125,53 @@ class DiscordBot(commands.Bot):
                 if image_url:
                     embed.set_image(url=image_url)  # Set the product image
 
-                await channel.send(embed=embed) # Send the message with the product details and image
+                await new_items_channel.send(embed=embed) # Send the message with the product details and image
 
                 # Wait 0.5 sec between messages
                 await asyncio.sleep(0.5)
+
+
+    async def load_welcome_messages(self) -> list[str]:
+        """Methdod to load welcome messages from a text file."""
+        try:
+            with open(WELCOME_MESSAGES_FILE_PATH, 'r', encoding='utf-8') as file:
+                return [line.strip() for line in file if line.strip()] # Returns a list of non-empty lines
+        except FileNotFoundError:
+            self.logger.error(f"Welcome messages file not found: {WELCOME_MESSAGES_FILE_PATH}")
+            return []
+        except Exception as e:
+            self.logger.error(f"An error occurred while loading welcome messages: {e}")
+            return []
+
+
+    async def on_member_join(self, member: discord.Member) -> None:
+        """Send a welcome message when a new member joins the server."""
+        guild = member.guild
+        welcome_channel = None
+
+        # Load welcome messages list from the file
+        welcome_messages = await self.load_welcome_messages()
+
+        if not welcome_messages or len(welcome_messages) == 0:
+            self.logger.warning("No welcome messages found from the file.")
+            welcome_messages = [f"Welcome to the Jirai Sweeties server, {member.mention}! We're glad to have you here!"]
+
+        for channel in guild.text_channels:
+            if self.bot_settings["welcome_channel_name"].lower() in channel.name.lower():
+                welcome_channel = channel
+                break
+
+        if welcome_channel:
+            try:
+                # Select a random welcome message and format it with the member mention
+                base_message = random.choice(welcome_messages)
+                welcome_message = base_message.format(member=member.mention)
+                await asyncio.sleep(3)
+                await welcome_channel.send(welcome_message)
+            except discord.Forbidden:
+                self.logger.warning(f"Bot does not have permission to send messages in the channel: {welcome_channel.name}")
+            except Exception as e:
+                self.logger.error(f"An error occurred while sending a welcome message: {e}")
+        else:
+            self.logger.warning("Welcome channel not found.")
+
