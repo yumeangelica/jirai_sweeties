ed2c7257df890d551e9e85bbe7489c071b8ba80a
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2025-01-23 00:08:50 +0200

    Enhance DiscordDatabase class: improve type annotations, add optional type for get_user method, and ensure proper imports

--- bot/discord_database.py
+++ bot/discord_database.py
@@ -4,9 +4,11 @@ from datetime import datetime
 import asyncio
 import sys
 import logging
+from typing import Optional
 
-from utils.data_directory_helper import ensure_directory_exists
 sys.path.append(os.path.dirname(os.path.dirname(__file__))) # Add the project root directory to the path
+from utils.data_directory_helper import ensure_directory_exists
+from bot.types import DiscordUserDataType
 
 # Get to the root directory
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # Root project directory
@@ -15,12 +17,10 @@ DATA_DIR = os.path.join(ROOT_DIR, "data")  # Data directory
 ensure_directory_exists(DATA_DIR)  # Ensure that the data directory exists
 
 SQLITE_DISCORD_DB_FILE = os.path.join(DATA_DIR, "discord_db.sqlite")  # SQLite database file
-print(SQLITE_DISCORD_DB_FILE)
-
 
 class DiscordDatabase:
     """Manage the bot database."""
-    def __init__(self):
+    def __init__(self) -> None:
         self.logger = logging.getLogger("BotDatabase")
         self.store_db_file_path = SQLITE_DISCORD_DB_FILE
         self.db_name = "Discord Database"
@@ -34,6 +34,7 @@ class DiscordDatabase:
         except self.conn.Error as e:
             self.logger.error(f"Failed to connect to the database: {e}")
             self.logger.error(f"Error connecting to the database: {e}")
+
 
     def init_database(self) -> None:
         """Initialize the database."""
@@ -54,11 +55,13 @@ class DiscordDatabase:
             self.conn.rollback()
             return
 
+
     async def close_connection(self) -> None:
         """Close the database connection."""
         self.logger.info("Closing database connection...")
         if self.conn:
             self.conn.close()
+
 
     async def add_user(self, user_id: int, username: str) -> None:
         """Add a user to the database."""
@@ -75,11 +78,12 @@ class DiscordDatabase:
             self.conn.rollback()
             return
 
-    async def get_user(self, user_id: int, username: str) -> dict | None:
+
+    async def get_user(self, user_id: int, username: str) -> Optional[DiscordUserDataType]:
         """Get a user from the database."""
         try:
             self.logger.info(f"Getting user {user_id} from the database...")
-            user = self.cursor.execute("""
+            user: DiscordUserDataType = self.cursor.execute("""
                 SELECT * FROM Users WHERE id = ?
                 """, (user_id,)).fetchone()
 
@@ -96,7 +100,7 @@ class DiscordDatabase:
                 self.conn.commit()
                 user["username"] = username
 
-            user_dict = dict(user)
+            user_dict: Optional[DiscordUserDataType] = user
 
             self.logger.info(f"User {user_id} retrieved from the database.")
             return user_dict
